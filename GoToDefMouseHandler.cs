3e140e3e2193f2c805437f586e6b41942be347c7
Kirill Prazdnikov <kirill.prazdnikov@jetbrains.com>
2016-12-21 23:21:46 +0300

    project for 2017 ititial commit

--- GoToDefMouseHandler.cs
+++ GoToDefMouseHandler.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.ComponentModel.Composition;
-using System.Runtime.InteropServices;
+//using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
 using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.Editor;
+//using Microsoft.VisualStudio.Editor;
 using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Text;
